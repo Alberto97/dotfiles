6cfe6ca73ec453b4a6690c8bcebc29c2bb73fc96
Alberto Pedron <albertop2197@gmail.com>
2022-02-20 22:41:30 +0100

    Add fish user config

---
+++ .config/fish/conf.d/user_config.fish
@@ -0,0 +1,6 @@
+set EXT_DISK_PATH /run/media/$USER/Crucial_500GB
+
+fish_add_path $EXT_DISK_PATH/flutter/bin
+fish_add_path $EXT_DISK_PATH/Android/Sdk/platform-tools
+
+set ANDROID_HOME $EXT_DISK_PATH/Android/Sdk
